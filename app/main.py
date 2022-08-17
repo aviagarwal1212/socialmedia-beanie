efda763bb3f3999e31262684bf29d892e79d83cb
Avi Agarwal <aviagarwal1212@gmail.com>
2022-08-17 14:27:44 +0530

    Added CORS-handling code

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .config.database import init_db
 from .routers import auth, post, user, vote
@@ -8,6 +9,15 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
